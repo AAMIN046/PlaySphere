e5fabde3cbc63337b74792db33643e04f4647b82
AAMIN046 <aaminhossain16@gmail.com>
2026-02-12 13:46:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 // Loader
-window.onload = function() {
-    document.getElementById("loader").style.display = "none";
-};
+window.onload = function(){ document.getElementById("loader").style.display="none"; };
 
-// Dark Mode
-function toggleDarkMode() {
-    document.body.classList.toggle("dark");
-}
+// Dark Mode Toggle
+function toggleDarkMode(){ document.body.classList.toggle("dark"); }
 
 // Search
-document.getElementById("search").addEventListener("keyup", function() {
+document.getElementById("search").addEventListener("keyup", function(){
     let value = this.value.toLowerCase();
     let cards = document.querySelectorAll(".game-card");
-
     cards.forEach(card => {
         let name = card.getAttribute("data-name");
         card.style.display = name.includes(value) ? "inline-block" : "none";
